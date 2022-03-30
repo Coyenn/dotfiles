18fdc40737add1a9f1d280b5994474077a47f59f
Coyenn <timrittermail@gmail.com>
2022-03-30 12:45:23 +0200

    Cleanup emacs config

--- emacs/config.el
+++ emacs/config.el
@@ -68,27 +68,6 @@
 ;; Show only one active window when opening multiple files at the same time.
 (add-hook 'window-setup-hook 'delete-other-windows)
 
-;; Remove *scratch* from buffer after the mode has been set
-(defun remove-scratch-buffer ()
-  (if (get-buffer "*scratch*")
-      (kill-buffer "*scratch*")))
-
-(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
-
-;; Remove *messages* buffer
-(setq-default message-log-max nil)
-(kill-buffer "*Messages*")
-
-;; Remove *Completions* buffer
-(add-hook 'minibuffer-exit-hook
-      '(lambda ()
-         (let ((buffer "*Completions*"))
-           (and (get-buffer buffer)
-                (kill-buffer buffer)))))
-
-;; Don't show *Buffer list* when opening multiple files at the same time.
-(setq inhibit-startup-buffer-menu t)
-
 ;----------------------------------------------------------------
 ; VISUAL
 ;----------------------------------------------------------------
